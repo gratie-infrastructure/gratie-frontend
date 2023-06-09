fc5cd06ee721429fb90e87ec4354928b4d9e9852
arun karthik <arun.karthik11@gmail.com>
2023-06-09 22:00:08 +0530

    cleanup solana code

--- pages/company.tsx
+++ pages/company.tsx
@@ -1,9 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import NavBarWallet from './navBarWallet'
+import Head from "next/head";
+import NavBarNFT from "./navBarNFT";
 
-import Company from '@/src/components/company/index';
+import Company from "@/src/components/company/index";
 
 export default function Home() {
   return (
@@ -14,8 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBarWallet/>
-      <Company/>
+      <NavBarNFT />
+      <Company />
     </>
-  )
+  );
 }
