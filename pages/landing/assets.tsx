0aaa62af3390b128b79a9fb863d663613db606c0
SakshiShah29 <sakshi.shah299@gmail.com>
2023-06-18 09:33:27 +0530

    effects on process made

---
+++ pages/landing/assets.tsx
@@ -0,0 +1,289 @@
+export const process = (
+  <svg
+    width="1792"
+    height="477"
+    viewBox="0 0 1792 477"
+    fill="none"
+    style={{ marginLeft: "-260px" }}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M516.106 135.777C516.106 119.128 512.827 102.643 506.456 87.2614C500.085 71.8802 490.747 57.9044 478.974 46.1321C467.202 34.3597 453.226 25.0214 437.845 18.6503C422.464 12.2792 405.978 9 389.33 9C372.681 9 356.195 12.2792 340.814 18.6503C325.433 25.0214 311.457 34.3598 299.685 46.1321C287.912 57.9044 278.574 71.8802 272.203 87.2614C265.832 102.643 262.553 119.128 262.553 135.777"
+      stroke="url(#paint0_radial_1643_1316)"
+      stroke-width="16.1454"
+    />
+    <path
+      d="M1023.21 135.777C1023.21 119.128 1019.93 102.643 1013.56 87.2614C1007.19 71.8802 997.854 57.9044 986.082 46.1321C974.309 34.3598 960.334 25.0214 944.952 18.6503C929.571 12.2792 913.086 9 896.437 9C879.788 9 863.303 12.2792 847.922 18.6503C832.54 25.0214 818.565 34.3598 806.792 46.1321C795.02 57.9044 785.682 71.8802 779.31 87.2614C772.939 102.643 769.66 119.128 769.66 135.777"
+      stroke="url(#paint1_radial_1643_1316)"
+      stroke-width="16.1454"
+    />
+    <path
+      d="M1530.32 135.777C1530.32 119.128 1527.04 102.643 1520.67 87.2614C1514.29 71.8802 1504.96 57.9044 1493.18 46.1321C1481.41 34.3598 1467.44 25.0214 1452.05 18.6503C1436.67 12.2792 1420.19 9 1403.54 9C1386.89 9 1370.41 12.2792 1355.02 18.6503C1339.64 25.0214 1325.67 34.3598 1313.89 46.1321C1302.12 57.9044 1292.78 71.8802 1286.41 87.2614C1280.04 102.643 1276.76 119.128 1276.76 135.777"
+      stroke="url(#paint2_radial_1643_1316)"
+      stroke-width="16.1454"
+    />
+    <path
+      d="M516.107 132.495C516.107 149.143 519.386 165.629 525.757 181.01C532.128 196.391 541.466 210.367 553.239 222.139C565.011 233.912 578.987 243.25 594.368 249.621C609.749 255.992 626.235 259.271 642.883 259.271C659.532 259.271 676.017 255.992 691.399 249.621C706.78 243.25 720.756 233.912 732.528 222.139C744.3 210.367 753.639 196.391 760.01 181.01C766.381 165.629 769.66 149.143 769.66 132.495"
+      stroke="url(#paint3_radial_1643_1316)"
+      stroke-width="16.1454"
+    />
+    <path
+      d="M1023.21 129.754C1023.21 146.403 1026.49 162.889 1032.86 178.27C1039.23 193.651 1048.57 207.627 1060.34 219.399C1072.11 231.171 1086.09 240.51 1101.47 246.881C1116.85 253.252 1133.34 256.531 1149.99 256.531C1166.63 256.531 1183.12 253.252 1198.5 246.881C1213.88 240.51 1227.86 231.171 1239.63 219.399C1251.4 207.627 1260.74 193.651 1267.11 178.27C1273.48 162.889 1276.76 146.403 1276.76 129.754"
+      stroke="url(#paint4_radial_1643_1316)"
+      stroke-width="16.1454"
+    />
+    <path
+      d="M262.555 135.777V342.039"
+      stroke="url(#paint5_linear_1643_1316)"
+      stroke-width="16.1456"
+    />
+    <path
+      d="M1530.32 135.777V342.039"
+      stroke="url(#paint6_linear_1643_1316)"
+      stroke-width="16.1456"
+    />
+    <path
+      d="M8.99914 342.039C8.99914 358.687 12.2783 375.173 18.6495 390.554C25.0206 405.935 34.3589 419.911 46.1312 431.683C57.9035 443.456 71.8793 452.794 87.2606 459.165C102.642 465.536 119.127 468.815 135.776 468.815C152.424 468.815 168.91 465.536 184.291 459.165C199.673 452.794 213.648 443.456 225.421 431.683C237.193 419.911 246.531 405.935 252.902 390.554C259.274 375.173 262.553 358.687 262.553 342.039"
+      stroke="url(#paint7_radial_1643_1316)"
+      stroke-width="16.1454"
+    />
+    <path
+      d="M1530.32 342.04C1530.32 358.688 1533.6 375.174 1539.97 390.555C1546.34 405.936 1555.68 419.912 1567.45 431.684C1579.22 443.457 1593.2 452.795 1608.58 459.166C1623.96 465.537 1640.44 468.816 1657.09 468.816C1673.74 468.816 1690.23 465.537 1705.61 459.166C1720.99 452.795 1734.97 443.457 1746.74 431.684C1758.51 419.912 1767.85 405.936 1774.22 390.555C1780.59 375.174 1783.87 358.688 1783.87 342.04"
+      stroke="url(#paint8_radial_1643_1316)"
+      stroke-width="16.1454"
+    />
+    <defs>
+      <radialGradient
+        id="paint0_radial_1643_1316"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(389.33 140.784) rotate(-2.11442) scale(135.702 140.724)"
+      >
+        <stop stop-color="#006700" />
+        <stop offset="0.894151" stop-color="#02591F" />
+        <stop offset="0.915385" stop-color="#00A514" />
+        <stop offset="0.931885" stop-color="#00DE01" />
+        <stop offset="0.958084" stop-color="#6DF16E" />
+        <stop offset="1" stop-color="#308131" />
+      </radialGradient>
+      <radialGradient
+        id="paint1_radial_1643_1316"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(896.437 140.784) rotate(-2.11442) scale(135.702 140.724)"
+      >
+        <stop stop-color="#006700" />
+        <stop offset="0.894151" stop-color="#02591F" />
+        <stop offset="0.915385" stop-color="#00A514" />
+        <stop offset="0.931885" stop-color="#00DE01" />
+        <stop offset="0.958084" stop-color="#6DF16E" />
+        <stop offset="1" stop-color="#308131" />
+      </radialGradient>
+      <radialGradient
+        id="paint2_radial_1643_1316"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1403.54 140.784) rotate(-2.11442) scale(135.702 140.724)"
+      >
+        <stop stop-color="#006700" />
+        <stop offset="0.894151" stop-color="#02591F" />
+        <stop offset="0.915385" stop-color="#00A514" />
+        <stop offset="0.931885" stop-color="#00DE01" />
+        <stop offset="0.958084" stop-color="#6DF16E" />
+        <stop offset="1" stop-color="#308131" />
+      </radialGradient>
+      <radialGradient
+        id="paint3_radial_1643_1316"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(642.883 132.494) rotate(180) scale(147.54 153.001)"
+      >
+        <stop offset="0.0970319" stop-color="#6DF16E" />
+        <stop offset="0.787446" stop-color="#093809" />
+        <stop offset="0.839212" stop-color="#6DF16E" />
+        <stop offset="0.861774" stop-color="#00DE01" />
+        <stop offset="0.897746" stop-color="#006729" />
+        <stop offset="0.941593" stop-color="#093809" />
+      </radialGradient>
+      <radialGradient
+        id="paint4_radial_1643_1316"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1149.99 129.754) rotate(180) scale(147.54 153.001)"
+      >
+        <stop offset="0.0970319" stop-color="#6DF16E" />
+        <stop offset="0.787446" stop-color="#093809" />
+        <stop offset="0.839212" stop-color="#6DF16E" />
+        <stop offset="0.861774" stop-color="#00DE01" />
+        <stop offset="0.897746" stop-color="#006729" />
+        <stop offset="0.941593" stop-color="#093809" />
+      </radialGradient>
+      <linearGradient
+        id="paint5_linear_1643_1316"
+        x1="273.483"
+        y1="146.652"
+        x2="250.855"
+        y2="146.652"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.155613" stop-color="#02591F" />
+        <stop offset="0.223234" stop-color="#017A1A" />
+        <stop offset="0.469388" stop-color="#07DF08" />
+        <stop offset="0.673303" stop-color="#67E467" />
+        <stop offset="0.909415" stop-color="#3F9D40" />
+      </linearGradient>
+      <linearGradient
+        id="paint6_linear_1643_1316"
+        x1="1519.39"
+        y1="146.652"
+        x2="1542.02"
+        y2="146.652"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.155613" stop-color="#02591F" />
+        <stop offset="0.223234" stop-color="#017A1A" />
+        <stop offset="0.469388" stop-color="#07DF08" />
+        <stop offset="0.673303" stop-color="#67E467" />
+        <stop offset="0.909415" stop-color="#3F9D40" />
+      </linearGradient>
+      <radialGradient
+        id="paint7_radial_1643_1316"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(135.776 342.038) scale(149.777 155.321)"
+      >
+        <stop offset="0.153628" stop-color="#6DF16E" />
+        <stop offset="0.704578" stop-color="#093809" />
+        <stop offset="0.817339" stop-color="#6DF16E" />
+        <stop offset="0.851652" stop-color="#00DE01" />
+        <stop offset="0.891616" stop-color="#006729" />
+        <stop offset="0.941593" stop-color="#093809" />
+      </radialGradient>
+      <radialGradient
+        id="paint8_radial_1643_1316"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1657.09 342.039) scale(149.777 155.321)"
+      >
+        <stop offset="0.153628" stop-color="#6DF16E" />
+        <stop offset="0.704578" stop-color="#093809" />
+        <stop offset="0.817339" stop-color="#6DF16E" />
+        <stop offset="0.851652" stop-color="#00DE01" />
+        <stop offset="0.891616" stop-color="#006729" />
+        <stop offset="0.941593" stop-color="#093809" />
+      </radialGradient>
+    </defs>
+  </svg>
+);
+
+export const processCircle = (
+  <svg
+    width="189"
+    height="189"
+    viewBox="0 0 189 189"
+    fill="none"
+    // style={{boxShadow: "0px 0px 47.4427px 9.03671px #32D94B, inset -7.03775px 17.8014px 19.8713px #000000, inset 9.52167px -9.52167px 52.9902px -7.45174px rgba(223, 223, 223, 0.28)",borderRadius:"100px"}}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_ii_1585_1501)">
+      <circle cx="94.0368" cy="94.0368" r="94.0368" fill="#0E1803" />
+    </g>
+    <circle
+      cx="94.0368"
+      cy="94.0368"
+      r="88.448"
+      stroke="url(#paint0_radial_1585_1501)"
+      stroke-width="11.1776"
+    />
+    <defs>
+      <filter
+        id="filter0_ii_1585_1501"
+        x="-7.03775"
+        y="-9.52167"
+        width="204.634"
+        height="215.396"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feMorphology
+          radius="7.45174"
+          operator="dilate"
+          in="SourceAlpha"
+          result="effect1_innerShadow_1585_1501"
+        />
+        <feOffset dx="9.52167" dy="-9.52167" />
+        <feGaussianBlur stdDeviation="26.4951" />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.875 0 0 0 0 0.875 0 0 0 0 0.875 0 0 0 0.28 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="shape"
+          result="effect1_innerShadow_1585_1501"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="-7.03775" dy="17.8014" />
+        <feGaussianBlur stdDeviation="9.93565" />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect1_innerShadow_1585_1501"
+          result="effect2_innerShadow_1585_1501"
+        />
+      </filter>
+      <radialGradient
+        id="paint0_radial_1585_1501"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(94.0368 94.0368) scale(94.0368)"
+      >
+        <stop stop-color="#006700" />
+        <stop offset="0.886239" stop-color="#093809" />
+        <stop offset="0.944422" stop-color="#00DE01" />
+        <stop offset="0.974604" stop-color="#69E96A" />
+        <stop offset="0.983382" stop-color="#6DF16E" />
+        <stop offset="0.988395" stop-color="#6DF06E" />
+      </radialGradient>
+    </defs>
+  </svg>
+);
--- pages/landing/coreValue.tsx
+++ pages/landing/coreValue.tsx
@@ -2,17 +2,24 @@ import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
+import { process, processCircle } from './assets';
+
 export default function CoreValue(){
     return(
-        <Grid container spacing={10} sx={{ mt: 8 }} item xs={12}>
-          <Box>
+        <Grid container spacing={10} sx={{ mt: 8 }} item xs={12} className='w-100%'>
+       
           <Typography variant="h1" gutterBottom style={{ textAlign: 'center', width: '100%', fontFamily: 'ClashDisplay-Variable', fontWeight: '600', fontSize: '42px', marginBottom: '40px', marginTop: '40px' }}>
             Process
             </Typography>
-            <div className='image-container'>
-              <img alt="rewardBox" src='/images/process.png' className='image'/>
+            <div className='w-100% relative'>
+              {process}
+              <div className='circle-animation' style={{position:'absolute',top:"1605px",left:"140px",display:"inline-block",width:"fit-content",borderRadius:"100px" }}>{processCircle}</div>
+              <div className='circle-animation' style={{position:'absolute',top:"1605px",left:"395px",display:"inline-block",width:"fit-content",borderRadius:"100px"}}>{processCircle}</div>
+              <div className='circle-animation' style={{position:'absolute',top:"1605px",left:"645px",display:"inline-block",width:"fit-content",borderRadius:"100px"}}>{processCircle}</div>
+              <div className='circle-animation' style={{position:'absolute',top:"1605px",left:"900px",display:"inline-block",width:"fit-content",borderRadius:"100px"}}>{processCircle}</div>
+              <div className='circle-animation' style={{position:'absolute',top:"1605px",left:"1150px",display:"inline-block",width:"fit-content",borderRadius:"100px"}}>{processCircle}</div>
             </div>
-          </Box>
+          
         </Grid>
     );
 }
